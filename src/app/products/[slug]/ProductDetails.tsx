2f33e073b42b3dd4f9de7dd1d7f1ab9e8552ee9a
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2025-03-30 22:57:48 +0530

    feat: enhance product details page with media selection and pricing components; add quantity input and additional info sections

--- src/app/products/[slug]/ProductDetails.tsx
+++ src/app/products/[slug]/ProductDetails.tsx
@@ -1,10 +1,21 @@
 "use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import Badge from "@/components/ui/badge";
-import WixImage from "@/components/WixImage";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { checkInStock, findVariant } from "@/lib/utils";
 import { products } from "@wix/stores";
+import { InfoIcon } from "lucide-react";
 import { useState } from "react";
+import ProductMedia from "./ProductMedia";
 import ProductOptions from "./ProductOptions";
+import ProductPrice from "./ProductPrice";
 
 interface ProductDetailsProps {
   product: products.Product;
@@ -22,8 +33,6 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       }))
       ?.reduce((acc, curr) => ({ ...acc, ...curr }), {}) || {},
   );
-
-  console.log("selectedOptions", selectedOptions);
 
   const selectedVariant = findVariant(product, selectedOptions);
 
@@ -44,16 +53,13 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
 
   return (
     <div className="flex flex-col gap-10 md:flex-row lg:gap-20">
-      <div className="basis-2/5">
-        <WixImage
-          mediaIdentifier={product?.media?.mainMedia?.image?.url}
-          alt={product.media?.mainMedia?.image?.altText}
-          width={1000}
-          height={1000}
-          className="sticky top-0"
-        />
-      </div>
-
+      <ProductMedia
+        media={
+          !!selectedOptionsMedia?.length
+            ? selectedOptionsMedia
+            : product.media?.items
+        }
+      />
       <div className="basis-3/5 space-y-5">
         <div className="space-y-2.5">
           <h1 className="text-3xl font-bold lg:text-4xl">{product.name}</h1>
@@ -68,12 +74,54 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
             className="prose dark:prose-invert"
           />
         )}
-
+        <ProductPrice product={product} selectedVariant={selectedVariant} />
         <ProductOptions
           product={product}
           selectedOptions={selectedOptions}
           setSelectedOptions={setSelectedOptions}
         />
+        <div className="space-y-1.5">
+          <Label htmlFor="quantity">Quantity</Label>
+          <div className="flex items-center gap-2.5">
+            <Input
+              name="quantity"
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(Number(e.target.value))}
+              className="w-24"
+              disabled={!inStock}
+            />
+            {!!availableQuantity &&
+              (availableQuantityExceeded || availableQuantity < 10) && (
+                <span className="text-destructive">
+                  Only {availableQuantity} left in stock
+                </span>
+              )}
+          </div>
+        </div>
+        {!!product.additionalInfoSections?.length && (
+          <div className="mt-4 space-y-1.5 text-sm text-muted-foreground">
+            <span className="flex items-center gap-2">
+              <InfoIcon className="size-5" />
+              <span>Additional product information</span>
+            </span>
+            <Accordion type="multiple">
+              {product.additionalInfoSections.map((section) => (
+                <AccordionItem value={section.title || ""} key={section.title}>
+                  <AccordionTrigger>{section.title}</AccordionTrigger>
+                  <AccordionContent>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: section.description || "",
+                      }}
+                      className="prose text-sm text-muted-foreground dark:prose-invert"
+                    />
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
+        )}
       </div>
     </div>
   );
