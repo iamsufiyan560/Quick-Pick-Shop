f1ed38f0adbf6883befa8ab620ccd63dc3c3aa72
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2025-05-19 12:00:00 +0530

    feat: add MainNavigation component and integrate collections in Navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -6,22 +6,28 @@ import { getLoggedInMember } from "@/wix-api/members";
 import Image from "next/image";
 import Link from "next/link";
 import ShoppingCartButton from "./ShoppingCartButton";
+import { getCollections } from "@/wix-api/collections";
+import MainNavigation from "./MainNavigation";
 
 export default async function Navbar() {
   const wixClient = getWixServerClient();
 
-  const [cart, loggedInMember] = await Promise.all([
+  const [cart, loggedInMember, collections] = await Promise.all([
     getCart(wixClient),
     getLoggedInMember(wixClient),
+    getCollections(wixClient),
   ]);
 
   return (
     <header className="bg-background shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 p-5">
-        <Link href="/" className="flex items-center gap-4">
-          <Image src={logo} alt="Quick Pick Shop logo" width={40} height={40} />
-          <span className="text-xl font-bold">Quick Pick Shop</span>
-        </Link>
+        <div className="flex flex-wrap items-center gap-5">
+          <Link href="/" className="flex items-center gap-4">
+            <Image src={logo} alt="Flow Shop logo" width={40} height={40} />
+            <span className="text-xl font-bold">Flow Shop</span>
+          </Link>
+          <MainNavigation collections={collections} />
+        </div>
         <div className="flex items-center justify-center gap-5">
           <UserButton loggedInMember={loggedInMember} />
           <ShoppingCartButton initialData={cart} />
